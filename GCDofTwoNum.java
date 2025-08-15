6eb7bf3a89b18b092dff1936791561e190903307
kumarrohit8477 <kumarrohitt0976@gmail.com>
2025-08-15 23:30:25 +0530

    update the file

--- GCDofTwoNum.java
+++ GCDofTwoNum.java
@@ -1,3 +1,15 @@
 public class GCDofTwoNum {
+   public int gcd(int a, int b) {
+      if (b == 0) {
+         return a;
+      }
+      return gcd(b, a % b);
+   }
+   public static void main(String[] args) {
+      GCDofTwoNum gcdCalculator = new GCDofTwoNum();
+      int a = 8;
+      int b = 1;
+      System.out.println(gcdCalculator.gcd(a, b));
+   }
    
 }
