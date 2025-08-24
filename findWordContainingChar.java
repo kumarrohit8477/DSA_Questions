29b0f0800c4748e7947649ce7fe3b1e58b88f3ea
kumarrohit8477 <kumarrohitt0976@gmail.com>
2025-08-24 22:57:00 +0530

    add new file

--- findWordContainingChar.java
+++ findWordContainingChar.java
@@ -1,9 +1,26 @@
-//leetcode.com/problems/find-words-containing-character/description/?envType=problem-list-v2&envId=array
-
 import java.util.List;
+import java.util.ArrayList;
 
 public class findWordContainingChar {
-   public List<Integer> findWordsContaining(String[] words, char x) {
-        
+    public List<Integer> findWordsContaining(String[] words, char x) {
+        List<Integer> result = new ArrayList<>();
+
+        for (int i = 0; i < words.length; i++) {
+            if (words[i].indexOf(x) != -1) { // check if char exists in the word
+                result.add(i); // add index to result
+            }
+        }
+
+        return result;
+    }
+
+    // Test the function
+    public static void main(String[] args) {
+        findWordContainingChar obj = new findWordContainingChar();
+        String[] words = { "leet", "code", "chatgpt", "java" };
+        char x = 'e';
+
+        List<Integer> result = obj.findWordsContaining(words, x);
+        System.out.println(result); // Output: [0, 1] (leet, code contain 'e')
     }
 }
